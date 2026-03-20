bd4e4f22b83a0908f13986840ee9f3b6d7883746
Manfrinato <jamanfrinato@gmail.com>
2026-03-20 11:31:36 -0300

    aula 2 variaveis e operadores

---
+++ Aula-02/Aula02.py
@@ -0,0 +1,29 @@
+print("ola mundo")
+
+print(7+4)
+print("7+4")
+print("7"+"4")  #concatenação de strings
+
+#comentários de 1 linha
+
+'''
+Comentários de 
+múltiplas 
+linhas 
+'''
+
+# Variáveis
+
+Nome = "alexandre" #str
+Idade=26 #int
+peso=49.9 #float
+
+print(Nome, Idade, peso)
+print(f"Oiiii {Nome}!!!!")
+
+#Input - simulação de um forms no cmd
+nome = input("digita o seu nome:")
+idade = int(input("digita sua idade:"))
+
+print(nome, idade)
+print(idade + 1)
